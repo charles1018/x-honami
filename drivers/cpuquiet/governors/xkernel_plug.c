d8a896467fcc8ce300d88141d992d3638ebf0ff9
Alok Nandan Nikhil <nikhil.jan93@ufl.edu>
2014-10-29 20:45:03 -0400

    X-Plug: Update Copyright information

--- drivers/cpuquiet/governors/xkernel_plug.c
+++ drivers/cpuquiet/governors/xkernel_plug.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014 Alok Nandan Nikhil <aloknnikhil@gmail.com>. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
